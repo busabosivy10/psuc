6c0365157dc5cd1ad546fd625943cfb67efe9e6c
busabosivy10 <43909029+busabosivy10@users.noreply.github.com>
2020-11-04 21:33:35 +0800

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@ import Firebase from 'firebase'
 import 'firebase/firestore'
 
 let config = {
-	apiKey: "AIzaSyCjhHd2plnJniM-Q52Ixzg-m5iPjZkj8j8",
-    authDomain: "psuc-2d75e.firebaseapp.com",
-    databaseURL: "https://psuc-2d75e.firebaseio.com",
-    projectId: "psuc-2d75e",
-    storageBucket: "psuc-2d75e.appspot.com",
-    messagingSenderId: "268554209735"
+	apiKey: "AIzaSyBk8z_7YM8zN458Tp_oHKEhuAWJvtFVTKw",
+    authDomain: "psu-complaint.firebaseapp.com",
+    databaseURL: "https://psu-complaint.firebaseio.com",
+    projectId: "psu-complaint",
+    storageBucket: "psu-complaint.appspot.com",
+    messagingSenderId: "663383467818"
 }
 
 let firebaseApp = Firebase.initializeApp(config)
